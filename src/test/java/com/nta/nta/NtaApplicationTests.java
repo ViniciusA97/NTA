4189486aecb21283f2e3253ac0adefe435ce4672
Vinícius Andrade de Queiroz <viniciusqueiroz@cc.ci.ufpb.br>
2021-04-08 07:32:58 -0300

    Primeiro commit

---
+++ src/test/java/com/nta/nta/NtaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nta.nta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NtaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
