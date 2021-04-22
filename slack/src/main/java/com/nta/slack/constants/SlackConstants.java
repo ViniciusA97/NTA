c6b137e6149b9ee67d0e892f45ed9f10f61f8942
Vinicius Andrade <vinicius.queiroz@dce.ufpb.br>
2021-04-22 19:06:21 -0300

    Update SlackConstants.java

--- slack/src/main/java/com/nta/slack/constants/SlackConstants.java
+++ slack/src/main/java/com/nta/slack/constants/SlackConstants.java
@@ -2,8 +2,8 @@ package com.nta.slack.constants;
 
 public class SlackConstants {
 
-    public static final String CLIENT_ID = "1937169268710.1937181048214";
+    public static final String CLIENT_ID = "";
 
-    public static final String CLIENT_SECRET = "3ba3d0cb537002e4dd9f1c7f66491f1a";
+    public static final String CLIENT_SECRET = "";
 
 }
